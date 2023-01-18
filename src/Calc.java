dd7d271afd86469375c2ba9e598100f57ba06b40
Max Kargin <Asket-axe@mail.ru>
2023-01-18 18:55:33 +0300

    enter

--- src/Calc.java
+++ src/Calc.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Calc {
     public static void main(String[] args) {
 
+
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
         System.out.println(calc(input));

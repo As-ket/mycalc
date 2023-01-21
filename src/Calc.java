604019d37d25496371ca7d390c8417dc823ccd10
Max <Asket-axe@mail.ru>
2023-01-22 00:38:08 +0300

    final mycalc

--- src/Calc.java
+++ src/Calc.java
@@ -1,57 +1,48 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calc {
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
         System.out.println(calc(input));
     }
-        public static String calc(String input) {
-        int c = 0;
-        if (input.contains("/")) {
-            String[] in = input.split("/");
-            Rim one = Rim.valueOf(in[0]);
-            Rim two = Rim.valueOf(in[1]);
-            c = get1(one)/get1(two);
-        } else if (input. contains("*")) {
-            String[] in = input.split("[*]");
-            Rim one = Rim.valueOf(in[0]);
-            Rim two = Rim.valueOf(in[1]);
-            c = get1(one)*get1(two);
-        } else if (input.contains("+")) {
-            String[] in = input.split("[+]");
-            Rim one = Rim.valueOf(in[0]);
-            Rim two = Rim.valueOf(in[1]);
-            c = get1(one)+get1(two);
-        } else if (input.contains("-")) {
-            String[] in = input.split("-");
-            Rim one = Rim.valueOf(in[0]);
-            Rim two = Rim.valueOf(in[1]);
-            c = get1(one)-get1(two);
-        }
-        return Rim.values()[c].toString();
-    }
+        public static String calc(String input) throws IOException {
 
-    public static int get1 (Rim rim){
-        return rim.ordinal();
-    }
-   /* public static String calc(String input) {
         int c = 0;
-        if (input.contains("/")) {
-            String [] in = input.split("/");
-            c = Integer.parseInt(in[0]) / Integer.parseInt(in[1]);
-        } else if (input.contains("*")) {
-            String [] in = input.split("[*]");
-            c = Integer.parseInt(in[0]) * Integer.parseInt(in[1]);
-        } else if (input.contains("+")) {
-            String [] in = input.split("[+]");
-            c = Integer.parseInt(in[0]) + Integer.parseInt(in[1]);
-        } else if (input.contains("-")) {
-            String [] in = input.split("-");
-            c = Integer.parseInt(in[0]) - Integer.parseInt(in[1]);
+        int a;
+        int b;
+        String [] in = input.split(" ");
+        int spaces = input.length() - input.replace(" ", "").length();
+        if (spaces >= 3) {
+            throw new IOException();
         }
-        return Integer.toString(c);
-    }*/
+
+
+            if (Character.isDigit(in[0].charAt(0))) {
+            a = Integer.parseInt(in[0]);
+            b = Integer.parseInt(in[2]);
+            if (in[1].contains("/")) {
+                c = a / b;
+            } else if (in[1]. contains("*")) {
+                c = a * b;
+            } else if (in[1].contains("+")) {
+                c = a + b;
+            } else if (in[1].contains("-")) {
+                c = a - b;
+            } return Integer.toString(c);
+        } else {
+            a = Rim.valueOf(in[0]).ordinal(); // Rim.valueOf() - преобразование строки в объект Rim
+            b = Rim.valueOf(in[2]).ordinal(); // ordinal() - порядковый номер объекта Rim.
+            if (in[1].contains("/")) {
+                c = a / b;
+            } else if (in[1]. contains("*")) {
+                c = a * b;
+            } else if (in[1].contains("+")) {
+                c = a + b;
+            } else if (in[1].contains("-")) {
+                c = a - b;
+            } return Rim.values()[c].toString();
+    }
+    }
 }

e35990cbcb7fb12c1577b7dbc000870a820be9b3
Max <Asket-axe@mail.ru>
2023-01-22 12:18:43 +0300

    mycalc final

--- src/Calc.java
+++ src/Calc.java
@@ -5,44 +5,42 @@ public class Calc {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        System.out.println(calc(input));
-    }
-        public static String calc(String input) throws IOException {
-
-        int c = 0;
-        int a;
-        int b;
-        String [] in = input.split(" ");
         int spaces = input.length() - input.replace(" ", "").length();
         if (spaces >= 3) {
             throw new IOException();
         }
-
-
-            if (Character.isDigit(in[0].charAt(0))) {
+        System.out.println(calc(input));
+    }
+        public static String calc(String input) throws IOException {
+        int a, b, c;
+        String [] in = input.split(" ");
+        if (Character.isDigit(in[0].charAt(0))) {
             a = Integer.parseInt(in[0]);
             b = Integer.parseInt(in[2]);
-            if (in[1].contains("/")) {
-                c = a / b;
-            } else if (in[1]. contains("*")) {
-                c = a * b;
-            } else if (in[1].contains("+")) {
-                c = a + b;
-            } else if (in[1].contains("-")) {
-                c = a - b;
+            c = arithmetic(a, b, in[1]);
+            if (a > 10 || b > 10){
+                throw new IOException();
             } return Integer.toString(c);
         } else {
             a = Rim.valueOf(in[0]).ordinal(); // Rim.valueOf() - преобразование строки в объект Rim
             b = Rim.valueOf(in[2]).ordinal(); // ordinal() - порядковый номер объекта Rim.
-            if (in[1].contains("/")) {
-                c = a / b;
-            } else if (in[1]. contains("*")) {
-                c = a * b;
-            } else if (in[1].contains("+")) {
-                c = a + b;
-            } else if (in[1].contains("-")) {
-                c = a - b;
+            c = arithmetic(a, b, in[1]);
+            if (a > 10 || b > 10) {
+                throw new IOException();
             } return Rim.values()[c].toString();
+        }
     }
+    public static int arithmetic (int a, int b, String in){
+        int d = 0;
+        if (in.contains("/")) {
+            d = a / b;
+        } else if (in. contains("*")) {
+            d = a * b;
+        } else if (in.contains("+")) {
+            d = a + b;
+        } else if (in.contains("-")) {
+            d = a - b;
+        }
+        return d;
     }
 }

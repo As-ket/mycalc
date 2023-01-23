09001c0f51d5e12a4726e607a3d219012c15024b
Max <Asket-axe@mail.ru>
2023-01-23 21:01:13 +0300

    mycalc final

--- src/Calc.java
+++ src/Calc.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) throws IOException {
+        System.out.println("Введите математическую операцию, состоящую из двух операндов и одного оператора, разделенных пробелами. \nДопустимо использование чисел от 1 до 10 (I - X).");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
         int spaces = input.length() - input.replace(" ", "").length();
@@ -18,16 +19,17 @@ public class Calc {
             a = Integer.parseInt(in[0]);
             b = Integer.parseInt(in[2]);
             c = arithmetic(a, b, in[1]);
-            if (a > 10 || b > 10){
+            if (a > 10 || b > 10 || a < 1 || b < 1){
                 throw new IOException();
             } return Integer.toString(c);
         } else {
             a = Rim.valueOf(in[0]).ordinal(); // Rim.valueOf() - преобразование строки в объект Rim
             b = Rim.valueOf(in[2]).ordinal(); // ordinal() - порядковый номер объекта Rim.
             c = arithmetic(a, b, in[1]);
-            if (a > 10 || b > 10) {
+            if (a > 10 || b > 10 || c < 1) {
                 throw new IOException();
-            } return Rim.values()[c].toString();
+            }
+            return Rim.values()[c].toString();
         }
     }
     public static int arithmetic (int a, int b, String in){
